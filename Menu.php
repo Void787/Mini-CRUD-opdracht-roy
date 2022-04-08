bb9458a1b4df0d8ead713d5cadf1ce481aad3054
Void787 <1195841@student.roc-nijmegen.nl>
2022-04-08 10:40:12 +0200

    verder met de kaarten voor menu, odp veranderd.

--- Menu.php
+++ Menu.php
@@ -21,8 +21,8 @@
         <div class="card me-3" style="width: 18rem;">
           <img class="card-img-top" src="img/Schermafbeelding kleuren keuze.png" alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title"></h5>
+            <p class="card-text"><?php  ?></p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
--- odp.php
+++ odp.php
@@ -27,4 +27,17 @@
         echo $e->getMessage();
         die("sorry, database problem");
     }
+
+    $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    $result = mysqli_query(mysqli $dsn);
+
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+    } else {
+    echo "0 results";
+    }
+?>
 ?>
