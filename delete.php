498661943ed6c9e9f6b81b120eb609065692c146
Void787 <1195841@student.roc-nijmegen.nl>
2022-04-20 22:07:35 +0200

    delete.php verwijderen

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include_once 'odp.php';
-
-$sql = "DELETE FROM Producten where ID=id;";
-    $smt = $connect->prepare($sql);
-    $smt ->bindParam('id', $get('id'));
-    $smt ->execute();
-    header("location: setting.php")
-?>
